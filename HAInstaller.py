fcfbadf16e7a45a38686202875f9ed506fecd5cd
DarviL <davidlosantos89@gmail.com>
2021-06-04 15:04:43 +0200

    Update HAInstaller.py

--- HAInstaller.py
+++ HAInstaller.py
@@ -1,7 +1,24 @@
 from os import path, listdir, system as runsys
 import winreg
-import json
 from srctools import cmdseq
+import json
+
+
+
+AVAILABLE_GAMES = {
+	"Portal 2": "portal2",
+	"Alien Swarm": "asw",
+	"Black Mesa": "blackmesa",
+	"Counter-Strike Global Offensive": "csgo",
+	"Half-Life 2": "hl2",
+	"Garry's Mod": "gmod",
+	"Infra": "infra",
+	"Left 4 Dead": "l4d",
+	"Left 4 Dead 2": "left4dead2",
+	"Portal": "portal",
+	"Team Fortress 2": "tf2"
+}
+
 
 
 
@@ -14,14 +31,14 @@ def getSteamPath() -> str:
 		"""
 		Check if the filepath supplied is valid and actually contains Steam.
 		"""
-		STEAM_CONTENTS = (
+		STEAM_CONTENTS = {
 			'crashhandler.dll', 'crashhandler64.dll', 'CSERHelper.dll', 'd3dcompiler_46.dll', 'd3dcompiler_46_64.dll', 'GameOverlayRenderer.dll', 'GameOverlayRenderer64.dll',
 			'GfnRuntimeSdk.dll', 'icui18n.dll', 'icuuc.dll', 'libavcodec-58.dll', 'libavformat-58.dll', 'libavresample-4.dll', 'libavutil-56.dll', 'libfreetype-6.dll',
 			'libharfbuzz-0.dll', 'libswscale-5.dll', 'libx264-142.dll', 'openvr_api.dll', 'SDL2.dll', 'SDL2_ttf.dll', 'Steam.dll', 'Steam2.dll', 'steamclient.dll', 'steamclient64.dll',
 			'SteamOverlayVulkanLayer.dll', 'SteamOverlayVulkanLayer64.dll', 'SteamUI.dll', 'steamwebrtc.dll', 'steamwebrtc64.dll', 'tier0_s.dll', 'tier0_s64.dll', 'v8.dll', 'video.dll',
 			'VkLayer_steam_fossilize.dll', 'VkLayer_steam_fossilize64.dll', 'vstdlib_s.dll', 'vstdlib_s64.dll', 'zlib1.dll', 'GameOverlayUI.exe', 'steam.exe', 'steamerrorreporter.exe',
 			'steamerrorreporter64.exe', 'streaming_client.exe', 'uninstall.exe', 'WriteMiniDump.exe'
-		)
+		}
 
 		if path.isdir(filename):
 			for steamfile in STEAM_CONTENTS:
@@ -56,23 +73,8 @@ def getSteamPath() -> str:
 
 
 def selectGame():
-	AVAILABLE_GAMES = (
-		"Portal 2",
-		"Alien Swarm",
-		"Black Mesa",
-		"Counter-Strike Global Offensive",
-		"Half-Life 2",
-		"Garry's Mod",
-		"Infra",
-		"Left 4 Dead",
-		"Left 4 Dead 2",
-		"Portal",
-		"Team Fortress 2"
-	)
-
-	commonFolder = path.join(steamPath, "steamapps\common")
 	usingGames = []
-	for game in listdir(commonFolder):
+	for game in listdir(commonPath):
 		if game in AVAILABLE_GAMES:
 			usingGames.append(game)
 
@@ -85,9 +87,9 @@ def selectGame():
 	while True:
 		try:
 			usrInput = int(input())
-			if usrInput not in range(1, len(usingGames)):
-				print("\x1b[A\x1b[2K", end="")
-				continue
+			if usrInput not in range(1, len(usingGames) + 1):
+				raise ValueError
+			print("\x1b[A\x1b[2K", end="")
 			return usingGames[usrInput - 1]
 
 		except (ValueError, IndexError):
@@ -95,6 +97,25 @@ def selectGame():
 			pass
 
 	
+
+
+
+
+
+
+def selectCmdSeq(game):
+	cmdSeqPath = path.join(commonPath, game, "bin\CmdSeq.wc")
+	if path.isfile(cmdSeqPath):
+		with open(cmdSeqPath, "rb") as cmdfile:
+			data = dict(cmdseq.parse(cmdfile))
+			
+
+					
+					
+						
+	else:
+		print(f"Couldn't find the CmdSeq.wc file in the game '{game}'. Perhaps you forgot to launch Hammer for the first time?")
+		quit()
 	
 
 
@@ -108,10 +129,12 @@ def selectGame():
 
 
 steamPath = getSteamPath()
+commonPath = path.join(steamPath, "steamapps\common")
 runsys("")
 
 def main():
-	selectGame()
+	selectedGame = selectGame()
+	selectCmdSeq(selectedGame)
 	
 
 
